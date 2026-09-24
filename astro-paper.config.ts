3c1862493e8698d0016935ba53c225d184830ec8
Richard Bown <rich@xyglo.com>
2026-09-24 12:16:37 +0200

    update to jpg

--- astro-paper.config.ts
+++ astro-paper.config.ts
@@ -6,7 +6,7 @@ export default defineAstroPaperConfig({
     title: "Richard W. Bown - Author",
     shortTitle: "Richard W. Bown - Author",
     description: "Stories about what we build, what it costs, and the consequences of being human at work.",
-    ogImage: "og-rwb.png",
+    ogImage: "og-rwb.jpg",
     author: "Richard W. Bown",
     profile: "",
     lang: "en",
